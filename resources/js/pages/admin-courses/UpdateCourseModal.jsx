40811f91ccfc422b055ba3f46c2ad130d7287862
trado006 <trado006@gmail.com>
2022-01-22 18:55:23 +0700

    update

--- resources/js/pages/admin-courses/UpdateCourseModal.jsx
+++ resources/js/pages/admin-courses/UpdateCourseModal.jsx
@@ -1,11 +1,7 @@
 import { useState, useEffect } from 'react';
 import { Modal, message } from 'antd';
 
-<<<<<<< HEAD:resources/js/pages/admin-mycourses/AddCourseModal.jsx
-export default function App({ addCourse, isAddCourseModalVisible, setIsAddCourseModalVisible }) {
-=======
 export default function App({ courseId, updateCourse, setIsModalVisible }){
->>>>>>> trado:resources/js/pages/admin-courses/UpdateCourseModal.jsx
     var choosedFile = null;
     const [ course, setCourse ] = useState();
     
@@ -33,41 +29,14 @@ export default function App({ courseId, updateCourse, setIsModalVisible }){
         event.preventDefault();
         let data = event.target.elements;
         const formData = new FormData();
-<<<<<<< HEAD:resources/js/pages/admin-mycourses/AddCourseModal.jsx
-        if (!choosedFile) {
-            message.error("Course picture is required");
-            return false;
-=======
         if(choosedFile){
             formData.append('image', choosedFile, choosedFile.name);
->>>>>>> trado:resources/js/pages/admin-courses/UpdateCourseModal.jsx
         }
         formData.append('code', data.code.value);
         console.log(data.code.value);
         formData.append('name', data.name.value);
         formData.append('price', data.price.value);
         formData.append('description', data.description.value);
-<<<<<<< HEAD:resources/js/pages/admin-mycourses/AddCourseModal.jsx
-
-        API.post('/course', formData)
-            .then((res) => {
-                if (res.data.error_code) {
-                    message.error('view detail error in console');
-                    console.log(res.data);
-                    return;
-                }
-                let course = {
-                    id: res.data.course.id,
-                    name: res.data.course.name,
-                };
-                addCourse(course);
-                message.success("Add course successfully");
-                setIsAddCourseModalVisible(false);
-            }).catch((err) => {
-                message.error('View detai error in console');
-                console.log(err);
-            })
-=======
         axios.post(`/course/${courseId}`,formData)
         .then((res)=>{
             console.log(res.data);
@@ -87,7 +56,6 @@ export default function App({ courseId, updateCourse, setIsModalVisible }){
             message.error('View detai error in console');
             console.log(err);
         })
->>>>>>> trado:resources/js/pages/admin-courses/UpdateCourseModal.jsx
     }
 
     const changeFile = (event) => {
@@ -97,12 +65,7 @@ export default function App({ courseId, updateCourse, setIsModalVisible }){
             const reader = new FileReader();
 
             reader.onload = function (e) {
-<<<<<<< HEAD:resources/js/pages/admin-mycourses/AddCourseModal.jsx
-                document.getElementById('imageView').setAttribute('src', `${e.target.result}`);
-                // setImageSource('url(' + e.target.result + ')');
-=======
                 document.getElementById('imageView').setAttribute('src',`${e.target.result}`);
->>>>>>> trado:resources/js/pages/admin-courses/UpdateCourseModal.jsx
             }
             reader.readAsDataURL(input.files[0]);
         } else {
@@ -115,52 +78,6 @@ export default function App({ courseId, updateCourse, setIsModalVisible }){
         document.getElementById('fileInput').click();
     }
 
-<<<<<<< HEAD:resources/js/pages/admin-mycourses/AddCourseModal.jsx
-    return (
-        <>
-            <Modal
-                title={<span><i className="fa fa-book" /> Add course</span>}
-                onCancel={handleCancel}
-                footer={null}
-                visible={isAddCourseModalVisible}
-                zIndex={5}
-                width={600} >
-                <form className="row" onSubmit={onSubmit}>
-                    <div className="col-xl-6 col-lg-6 col-md-12">
-                        <div className="form-group mb-3">
-                            <label htmlFor="course-code">Code</label>
-                            <input id="course-code" name="code" type="text" className="form-control validate" required />
-                        </div>
-                        <div className="form-group mb-3">
-                            <label htmlFor="course-name">Name</label>
-                            <input id="course-name" name="name" type="text" className="form-control validate" required />
-                        </div>
-                        <div className="form-group mb-3">
-                            <label htmlFor="course-price">Price</label>
-                            <input id="course-price" name="price" type="number" min="1" className="form-control validate" required />
-                        </div>
-                    </div>
-                    <div className="col-xl-6 col-lg-6 col-md-12">
-                        <div className="border rounded p-1 bg-info">
-                            <img className="w-100" id="imageView" style={{ minHeight: '200px' }} />
-                        </div>
-                        <div className="custom-file mt-3 mb-3">
-                            <input id="fileInput" type="file" accept=".gif,.jpg,.jpeg,.png" style={{ display: 'none' }} onChange={changeFile} />
-                            <input type="button" className="btn btn-primary btn-block" value="UPLOAD COURSE IMAGE" onClick={onClickFileUpload} />
-                        </div>
-                    </div>
-                    <div className="col-12">
-                        <div className="mb-4">
-                            <label htmlFor="course-description">Description</label>
-                            <textarea id="course-description" name="description" className="form-control validate" rows="3" required></textarea>
-                        </div>
-                        <button type="submit" className="btn btn-primary btn-block text-uppercase">Add Product Now</button>
-                    </div>
-                </form>
-            </Modal>
-        </>
-    );
-=======
   return (
     <>
         <Modal
@@ -206,5 +123,4 @@ export default function App({ courseId, updateCourse, setIsModalVisible }){
         </Modal>
     </>
   );
->>>>>>> trado:resources/js/pages/admin-courses/UpdateCourseModal.jsx
 };
